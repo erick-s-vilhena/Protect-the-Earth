2c8d44d0d37c2936437b44127c3ac8d547e9f58a
erick-s-vilhena <erick.s.vilhena@gmail.com>
2025-05-21 13:46:27 -0300

    r5

---
+++ src/js/classes.js
@@ -0,0 +1,91 @@
+class Sprite{
+    constructor(x, y, radius, color){
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+
+    draw(){
+        ctx.beginPath()
+        ctx.arc(
+            this.x,     
+            this.y, 
+            this.radius, 
+            0, Math.PI*2,
+            false
+            )
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+}
+
+class Player extends Sprite{
+    constructor(x, y, radius, color){
+        super(x, y, radius, color)
+
+        this.coreRadius = radius / 6;
+
+        this.s1 = new Sphere(
+            this.x + Math.cos(0) * this.radius,
+            this.y + Math.sin(0) * this.radius,
+            2,
+            '#48fcff',
+            0.02,
+            0,
+            this
+        )
+
+        this.s2 = new Sphere(
+            this.x + Math.sin(0) * this.radius,
+            this.y + Math.cos(0) * this.radius,
+            2,
+            '#48fcff',
+            0.02,
+            Math.PI,
+            this
+        )
+    }
+
+    draw(){
+        ctx.beginPath()
+        ctx.arc(
+            this.x,     
+            this.y, 
+            this.coreRadius, 
+            0, Math.PI*2,
+            false
+            )
+        ctx.strokeStyle = this.color
+        ctx.stroke()
+    }
+
+    update(){
+        this.draw()
+        this.s1.update();
+        this.s2.update();
+    }
+}
+
+class Sphere extends Sprite{
+    constructor(x, y, radius, color, angleUpdateValue, angle, player){
+        super(x, y, radius, color)
+
+        this.angleUpdateValue = angleUpdateValue
+        this.player = player
+        this.angle = angle
+    }
+
+    update(){
+        this.draw()
+
+        this.angle += this.angleUpdateValue
+
+        if(Math.abs(this.angle) >= Math.PI*2){
+            this.angle = 0
+        }
+
+        this.x = this.player.x + Math.cos(this.angle) * (this.player.radius + 20)
+        this.y = this.player.y + Math.sin(this.angle) * (this.player.radius + 20)
+    }
+}
---
+++ src/script.js
@@ -0,0 +1,23 @@
+const cnv = document.getElementById('myCanvas');
+
+cnv.width = innerWidth;
+cnv.height = innerHeight;
+
+const ctx = cnv.getContext('2d')
+
+const player = new Player(cnv.width/2, cnv.height/2, 30, '#48fcff')
+
+function loop(){
+    requestAnimationFrame(loop, cnv);
+
+    update();
+}
+
+function update(){
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    ctx.fillRect(0, 0, cnv.width, cnv.height)
+
+    player.update()
+}
+
+loop();
